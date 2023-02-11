a6d0cfef7a63f03acfd8ea8340f289ee3591d1d5
Aj Hadi <me@ajhadi.space>
2023-02-11 21:50:14 +0700

    Add DataContext

---
+++ DataContext/DataContext.cs
@@ -0,0 +1,20 @@
+global using Microsoft.EntityFrameworkCore;
+
+namespace FoodOrderAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        private readonly IConfiguration configuration;
+        public DataContext(DbContextOptions<DataContext> options,IConfiguration configuration) : base(options)
+        {
+            this.configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            var db = configuration.GetValue<string>("ConnectionStrings:FoodOrderDb");
+            optionsBuilder.UseSqlServer(configuration.GetValue<string>("ConnectionStrings:FoodOrderDb"));
+        }
+    }
+}
