36123e5ed40a54908b1c909452105c3cb82a680b
Aj Hadi <me@ajhadi.space>
2023-02-13 22:34:51 +0700

    Delete unused code

--- Services/OrderService/IOrderService.cs
+++ Services/OrderService/IOrderService.cs
@@ -6,12 +6,14 @@ namespace FoodOrderAPI.Services.OrderService
     {
         /// <summary> Create new order </summary>
         Task<ServiceStatus> CreateOrder(OrderDTO request, Guid userId);
+        /// <summary> Get single order by Id </summary>
+        Task<ServiceStatus<Order>> GetOrder(Guid id);
+        /// <summary> Get active order by Table Id </summary>
+        Task<ServiceStatus<Order>> GetActiveOrder(int tableId);
         /// <summary> Get list of order </summary>
         Task<ServiceStatus<List<Order>>> GetOrders(OrderQueryDTO param, Guid userId);
         /// <summary> Get employee's order activities </summary>
         Task<ServiceStatus<List<Order>>> GetOrderActivities(Guid userId);
-        /// <summary> Get single order by Id </summary>
-        Task<ServiceStatus<Order>> GetOrder(Guid id);
         /// <summary> Update order's detail </summary>
         Task<ServiceStatus> UpdateOrder(Guid id, OrderDetailDTO request, Guid userId);
         /// <summary> Update order's items </summary>
